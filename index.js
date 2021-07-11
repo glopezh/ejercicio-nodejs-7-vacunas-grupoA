6083dbbb130d3475220f8a93265d7f581670f597
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-07-11 16:04:51 +0200

    persona corregida

--- index.js
+++ index.js
@@ -5,40 +5,31 @@ const { preguntasGenerales } = require("./inquirer");
 
 require("./db");
 
-preguntasGenerales();
-
-// (async () => {
-// Ejemplo creacion Persona
-// await crearPersona({
-//   dni: "43229678Z",
-//   vacuna: "60e828e1a599f53108e81fad",
-//   puntoVacunacion: {
-//     nombre: "Plaza Espña",
-//     localizacion: {
-//       coordenadas: [40.0, 50.25],
-//       direccion: "no",
-//     },
-//     vacunas: [],
-//   },
-//   dosis: [new Date(Date.parse("Aug 9, 1995")), new Date()],
-// });
-// await listarPersonas();
-// listarVacunas();
-// Ejemplo creacion ciudad
-// const ciudadNueva = {
-//   nombre: "Palma",
-//   puntosVacunacion: [
-//     {
-//       nombre: "Plaza España",
-//       localizacion: {
-//         coordenadas: [40.0, 50.25],
-//         direccion: "La calle mas chula de mallorca",
-//       },
-//       vacunas: [],
-//     },
-//   ],
-// };
-// await crearCiudad(ciudadNueva);
-//
-// await listarCiudades();
-// })();
+(async () => {
+  // Ejemplo creacion Persona
+  // await crearPersona({
+  //   dni: "43229678V",
+  //   vacuna: "60e828e1a599f53108e81fad",
+  //   puntoVacunacion: "60eaf5af89b3dc86a3faed43",
+  //   dosis: [new Date(Date.parse("Aug 9, 1995")), new Date()],
+  // });
+  // await listarPersonas();
+  // listarVacunas();
+  // Ejemplo creacion ciudad
+  // const ciudadNueva = {
+  //   nombre: "Palma",
+  //   puntosVacunacion: [
+  //     {
+  //       nombre: "Plaza España",
+  //       localizacion: {
+  //         coordenadas: [40.0, 50.25],
+  //         direccion: "La calle mas chula de mallorca",
+  //       },
+  //       vacunas: [],
+  //     },
+  //   ],
+  // };
+  // await crearCiudad(ciudadNueva);
+  //
+  // await listarCiudades();
+})();
