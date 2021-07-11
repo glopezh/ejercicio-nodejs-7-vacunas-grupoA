1495b6937246961b0f290b7f0f662b1583519e48
Rallge <geraldinelopz@gmail.com>
2021-07-11 02:07:49 +0200

    Probando el server y corrigiendo numero de puerto

--- api/apiServer.js
+++ api/apiServer.js
@@ -1,9 +1,14 @@
 const express = require("express");
 
 const app = express();
+const morgan =require ("morgan");
+
+// Middleware - morgan - ver lo que llega al servidor+
+
+app.use(morgan("dev"));
 
 // Starting the server
 
-app.listen(5000, () => {
+app.listen(3000, () => {
   console.log(`Server on port ${3000}`);
 });
